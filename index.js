32b655080041afb80ccf653ded81b8e27813a246
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-06-15 17:45:23 +0600

    add user post get API

--- index.js
+++ index.js
@@ -49,18 +49,21 @@ async function run() {
     });
 
     app.get("/check-admin", async (req, res) => {
-      const email = req.query?.email
-      const check =await isUserExist(email)
-      res.send({isAdmin:check?.isAdmin})      
+      const email = req.query?.email;
+      const check = await isUserExist(email);
+      res.send({ isAdmin: check?.isAdmin });
     });
 
-    app.post('/user-post', async(req, res) => {
-      const data = req.body
-      const result =await postCollection.insertOne(data)
-      console.log(result)
-      console.log(data)
-      res.send(result)
-    })
+    // user post related API
+    app.post("/user-post", async (req, res) => {
+      const data = req.body;
+      const result = await postCollection.insertOne(data);
+      // console.log(result)
+      // console.log(data)
+      res.send(result);
+    });
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
